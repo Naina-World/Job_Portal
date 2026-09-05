5571981e3664a23d3183f0624f90af3eca9e1ed8
Naina Kumari <nainakumarii734@gmail.com>
2026-09-05 14:14:35 +0530

    changes in client

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://job-portal-backend-4u0q.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
