a8b5925605b63ff719524e38987e4175d688e871
Naina Kumari <nainakumarii734@gmail.com>
2026-09-05 14:09:41 +0530

    changes in base url of api .js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-portal-backend-4u0q.onrender.com/api",
 });
 
 api.interceptors.request.use(
